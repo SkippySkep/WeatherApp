bd68a9777ecd6cd88017d074823fb3d30e7f7cc8
SkippySkep <joeyyeoss@gmail.com>
2025-07-05 17:15:52 -0700

    updated temperature output to imperial units

--- API.js
+++ API.js
@@ -12,7 +12,7 @@ enterKey.addEventListener('keydown', function(event) {
 async function getWeather() {
     let userLocation = document.getElementById('location').value;
     const apiKey = '5e64fb5b1f7242e155fcae707578e1a6';
-    const srcLink = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(userLocation)}&appid=${apiKey}&units=metric`;
+    const srcLink = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(userLocation)}&appid=${apiKey}&units=imperial`;
 
     try {
         const response = await fetch(srcLink);
